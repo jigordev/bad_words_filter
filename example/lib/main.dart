a1abb6edf9262bf436b96b2617f8c1ace76acb9f
jigordev <jigordev@gmail.com>
2025-05-12 20:12:07 -0300

    doc: add example

---
+++ example/lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:bad_words_filter/bad_words_filter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            BadWordsFilter(
+              child: TextField(
+                decoration: InputDecoration(labelText: 'Enter your text'),
+              ),
+              onDetected: (String text) async {
+                print('Bad word detected: $text');
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
