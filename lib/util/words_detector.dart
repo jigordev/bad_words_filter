1b5cadc135ff7af36191b94835128076c5c22e9d
jigordev <jigordev@gmail.com>
2025-05-13 19:52:02 -0300

    refactor: remove hardcoded wordlist

--- lib/util/words_detector.dart
+++ lib/util/words_detector.dart
@@ -1,7 +1,11 @@
 import 'wordlist.dart';
 
+String normalize(String input) {
+  return input.toLowerCase().replaceAll(RegExp(r'[^a-z0-9]'), '');
+}
+
 Future<bool> badWordsDetector(String text, String? wordListFile) async {
-  List<String> wordList =
-      wordListFile != null ? await loadWordList(wordListFile) : defaultWordList;
-  return wordList.any((word) => text.toLowerCase().contains(word));
+  final normalized = normalize(text);
+  final wordList = await loadWordList(wordListFile ?? 'assets/wordlist.txt');
+  return wordList.any((word) => normalized.contains(word));
 }
