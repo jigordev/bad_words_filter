c32297ca5c734856539000e91a9dfce21d9fbdc3
jigordev <jigordev@gmail.com>
2025-05-14 00:22:26 -0300

    fix: specify correct default wordlist path

--- lib/util/words_detector.dart
+++ lib/util/words_detector.dart
@@ -4,8 +4,10 @@ String normalize(String input) {
   return input.toLowerCase().replaceAll(RegExp(r'[^a-z0-9]'), '');
 }
 
-Future<bool> badWordsDetector(String text, String? wordListFile) async {
+Future<bool> badWordsDetector(String text, [String? wordListFile]) async {
   final normalized = normalize(text);
-  final wordList = await loadWordList(wordListFile ?? 'assets/wordlist.txt');
+  final wordList = await loadWordList(
+    wordListFile ?? 'packages/bad_words_moderator/assets/wordlist.txt',
+  );
   return wordList.any((word) => normalized.contains(word));
 }
