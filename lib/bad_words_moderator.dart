9bee947e5792b4e5276c08b8bc13dcf605af34e2
jigordev <jigordev@gmail.com>
2025-05-15 19:07:22 -0300

    refactor: switch to explicit controller injection in BadWordsModerator

--- lib/bad_words_moderator.dart
+++ lib/bad_words_moderator.dart
@@ -1,17 +1,20 @@
 import 'dart:async';
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:bad_words_moderator/util/words_detector.dart';
 
 class BadWordsModerator extends StatefulWidget {
   final Widget child;
+  final List<TextEditingController> controllers;
   final String? wordListFile;
-  final Future<bool> Function(String text, String? wordListFile) detector;
+  final Future<bool> Function(String text, [String? wordListFile]) detector;
   final Future<void> Function(TextEditingController controller) onDetected;
   final Duration debounce;
 
   const BadWordsModerator({
     super.key,
     required this.child,
+    required this.controllers,
     required this.onDetected,
     this.wordListFile,
     this.detector = badWordsDetector,
@@ -28,72 +31,72 @@ class _BadWordsModeratorState extends State<BadWordsModerator> {
   final Map<TextEditingController, String> _lastValues = {};
 
   @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    _setupListener();
-  }
-
-  void _setupListener() {
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      for (var entry in _listeners.entries) {
-        entry.key.removeListener(entry.value);
-      }
-      _listeners.clear();
-      _lastValues.clear();
-
-      final editables = _findEditableTexts(context);
-      for (final editable in editables) {
-        final controller = editable.controller;
-
-        void badWordsListener() {
-          _debounceTimers[controller]?.cancel();
-          _debounceTimers[controller] = Timer(widget.debounce, () async {
-            final text = controller.text;
-            if (text != _lastValues[controller]) {
-              _lastValues[controller] = text;
-              final hasBadWords =
-                  await widget.detector(text, widget.wordListFile);
-              if (hasBadWords) {
-                await widget.onDetected(controller);
-              }
-            }
-          });
-        }
-
-        final listener = badWordsListener;
-
-        controller.addListener(listener);
-        _listeners[controller] = listener;
-        _lastValues[controller] = controller.text;
-      }
-    });
-  }
-
-  List<EditableText> _findEditableTexts(BuildContext context) {
-    List<EditableText> editables = [];
-    void visitor(Element element) {
-      if (element.widget is EditableText) {
-        editables.add(element.widget as EditableText);
-      } else {
-        element.visitChildren(visitor);
-      }
-    }
-
-    context.visitChildElements(visitor);
-    return editables;
+  void initState() {
+    super.initState();
+    assert(
+      widget.controllers.toSet().length == widget.controllers.length,
+      'controllers list must not contain duplicates.',
+    );
+    _setupListeners();
   }
 
   @override
-  void dispose() {
-    for (var timer in _debounceTimers.values) {
-      timer.cancel();
+  void didUpdateWidget(covariant BadWordsModerator oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    assert(
+      widget.controllers.toSet().length == widget.controllers.length,
+      'controllers list must not contain duplicates.',
+    );
+    if (!const ListEquality()
+        .equals(oldWidget.controllers, widget.controllers)) {
+      _clearListeners();
+      _setupListeners();
     }
-    _debounceTimers.clear();
+  }
 
+  void _setupListeners() {
     for (var entry in _listeners.entries) {
       entry.key.removeListener(entry.value);
     }
     _listeners.clear();
+    _lastValues.clear();
+
+    for (final controller in widget.controllers) {
+      void listener() {
+        _debounceTimers[controller]?.cancel();
+        _debounceTimers[controller] = Timer(widget.debounce, () async {
+          final text = controller.text;
+          if (text != _lastValues[controller]) {
+            _lastValues[controller] = text;
+            final hasBadWords =
+                await widget.detector(text, widget.wordListFile);
+            if (hasBadWords) {
+              await widget.onDetected(controller);
+            }
+          }
+        });
+      }
+
+      controller.addListener(listener);
+      _listeners[controller] = listener;
+      _lastValues[controller] = controller.text;
+    }
+  }
+
+  void _clearListeners() {
+    _debounceTimers.values.forEach((timer) => timer.cancel());
+    _debounceTimers.clear();
+
+    _listeners.forEach((controller, listener) {
+      controller.removeListener(listener);
+    });
+    _listeners.clear();
+    _lastValues.clear();
+  }
+
+  @override
+  void dispose() {
+    _clearListeners();
     super.dispose();
   }
 
